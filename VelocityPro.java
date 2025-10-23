d2513a629af380d52c065bd6c8b171e66fe88ad1
1dce <202529223+Sui-love-u@users.noreply.github.com>
2025-10-23 16:32:01 +0800

    Add files via upload
    NoSprint NoXZ

---
+++ VelocityPro.java
@@ -0,0 +1,95 @@
+package cn.xiaojie.mtf.module.impl.combat;
+
+import cn.xiaojie.mtf.event.EventTarget;
+import cn.xiaojie.mtf.module.Category;
+import cn.xiaojie.mtf.module.Module;
+import cn.xiaojie.mtf.value.impl.ModeValue;
+import cn.xiaojie.mtf.value.impl.NumberValue;
+import cn.xiaojie.mtf.event.packet.PacketEvent;
+import cn.xiaojie.mtf.event.player.UpdateEvent;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientboundDamageEventPacket;
+import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket;
+import net.minecraft.network.protocol.game.ServerboundInteractPacket;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
+
+public class VelocityPro extends Module {
+    public final ModeValue mode = new ModeValue("Mode", "模式", new String[]{"GrimReduce"}, "GrimReduce");
+    public final ModeValue grimReduceMode = (ModeValue) new ModeValue("GrimReduce Mode", new String[]{"OneTime", "PerTick"}, "PerTick").setVisibility(() -> mode.is("GrimReduce"));
+    public final NumberValue attacks = (NumberValue) new NumberValue("Attack Count", 2, 1, 5, 1).setVisibility(() -> mode.is("GrimReduce"));
+
+    private Entity targetEntity;
+    private boolean velocityInput = false;
+    private int attackQueue = 0;
+
+    public VelocityPro() {
+        super("VelocityPro", Category.COMBAT);
+    }
+
+    @Override
+    public void onDisable() {
+        velocityInput = false;
+        targetEntity = null;
+        attackQueue = 0;
+    }
+
+    @EventTarget
+    public void onPacket(PacketEvent event) {
+        if (mc.level == null || mc.player == null) return;
+
+        Packet<?> packet = event.getPacket();
+
+        if (packet instanceof ClientboundDamageEventPacket damagePacket) {
+            if (damagePacket.entityId() == mc.player.getId()) {
+                velocityInput = true;
+            }
+        }
+
+        if (packet instanceof ClientboundSetEntityMotionPacket velocityPacket) {
+            if (velocityPacket.getId() != mc.player.getId()) {
+                return;
+            }
+
+            velocityInput = true;
+            targetEntity = KillAura.instance.getTarget();
+
+            if (mode.is("GrimReduce")) {
+                if (velocityInput) {
+                    velocityInput = false;
+                    attackQueue = attacks.getValue().intValue();
+                }
+            }
+        }
+    }
+
+    @EventTarget
+    public void onUpdate(UpdateEvent event) {
+        if (mc.player == null) return;
+
+        setSuffix(mode.getValue());
+
+        if (mc.player.hurtTime == 0) {
+            velocityInput = false;
+        }
+
+        if (mode.is("GrimReduce") && targetEntity != null && attackQueue > 0) {
+            if (grimReduceMode.is("OneTime")) {
+                for (; attackQueue >= 1; attackQueue--) {
+                    mc.getConnection().send(ServerboundInteractPacket.createAttackPacket(targetEntity, false));
+                    mc.player.setDeltaMovement(mc.player.getDeltaMovement().multiply(0.6, 1, 0.6));
+                    mc.player.setSprinting(false);
+                    mc.player.swing(InteractionHand.MAIN_HAND);
+                }
+            } else if (grimReduceMode.is("PerTick")) {
+                if (attackQueue >= 1) {
+                    mc.getConnection().send(ServerboundInteractPacket.createAttackPacket(targetEntity, false));
+                    mc.player.setDeltaMovement(mc.player.getDeltaMovement().multiply(0.6, 1, 0.6));
+                    mc.player.setSprinting(false);
+                    mc.player.swing(InteractionHand.MAIN_HAND);
+                }
+                attackQueue--;
+            }
+        }
+    }
+}
